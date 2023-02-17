ea3957b1bdc9396d40d9b6ba7318bce4a9006e95
Enzo Galati <enzogalati27664@gmail.com>
2023-02-17 09:38:21 -0300

    Atualizando Conta e Executavel

--- Conta.java
+++ Conta.java
@@ -2,24 +2,43 @@ package br.com.banco;
 
 public class Conta {
 
-	Cliente clinte;
+	Cliente cliente;
 	int numConta;
 	double saldo;
 
 	public Conta(Cliente clinte, int numConta, double saldo) {
-		this.clinte = clinte;
+		this.cliente = clinte;
 		this.numConta = numConta;
 		this.saldo = saldo;
 	}
-	
+
 	public void depositar(double valor) {
-		this.saldo = this.saldo+valor;
+		this.saldo = this.saldo + valor;
 		System.out.println("Depósito efetuado com sucesso");
 	}
-	
-	public void sacar(double valor) {
-		this.saldo = this.saldo-valor;
-		System.out.println("Saque efetuado com sucesso");
+
+	public boolean sacar(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("Saque efetuado com sucesso");
+			return true;
+		} else {
+			System.out.println("Não foi possivel realizar a operação");
+			return false;
+		}
+
+	}//
+
+	public void transferir(Conta conta, double valor) {
+		boolean teste = this.sacar(valor);
+		if (teste == true) {
+			conta.depositar(valor);
+		}
+	}//
+
+	public void consultaSaldo() {
+		System.out.println("Nome : " + this.cliente.nome);
+		System.out.println("Saldo : " + this.saldo);
 	}
 
 }
--- Executavel.java
+++ Executavel.java
@@ -3,14 +3,22 @@ package br.com.banco;
 public class Executavel {
 	
 	public static void main(String[] args) {
-		
 		Cliente clienteEnzo = new Cliente("Enzo", "Av Paula Ferreira","02/12/2002");
-		
+		Cliente borba = new Cliente("Borba" , "Rua da Aviação","02/11/2003");
 		Conta contaEnzo = new Conta(clienteEnzo, 1,0.0);
+		Conta conta2 = new Conta(borba,2,0.0);
 		
-		contaEnzo.depositar(10.000);
-		contaEnzo.sacar(5.000);
-		contaEnzo.depositar(50.000);
+		
+		
+		contaEnzo.depositar(10000);
+		conta2.depositar(5000);
+		
+		
+		contaEnzo.transferir(conta2, 1000);
+		
+		conta2.consultaSaldo();
+		
+		contaEnzo.consultaSaldo();
 		
 	}
 
